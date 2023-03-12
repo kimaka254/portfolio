7f864208ed938ee8acec1cafaec8bdd5910cb52c
kimaka254 <elsiekimaka02@gmail.com>
2023-03-12 12:54:43 +0300

    added scroll animation

--- main.js
+++ main.js
@@ -15,3 +15,36 @@ var typed = new Typed("#typed-text", {
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById("year").innerHTML = year;
+
+const sections = document.querySelectorAll('.section');
+
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide() {
+  sections.forEach(section => {
+    const slideInAt = (window.scrollY + window.innerHeight) - section.offsetHeight / 2;
+    const sectionBottom = section.offsetTop + section.offsetHeight;
+    const isHalfShown = slideInAt > section.offsetTop;
+    const isNotScrolledPast = window.scrollY < sectionBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      section.classList.add('is-active');
+    } else {
+      section.classList.remove('is-active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
