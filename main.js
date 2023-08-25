742987b1fe5ba577d4ca82cd3e38a399769ed855
kimaka254 <elsiekimaka02@gmail.com>
2023-08-25 15:56:52 +0300

    added services and updated resume

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var typed = new Typed("#typed-text", {
     strings: [
       "Elsy Marion Kimaka",
       "I'm a frontend web developer",
-      "I'm a data scientist.",
+      "I'm a data Analyst.",
+      "I'm a Machine Learning Engineer.",
     ],
     typeSpeed: 50,
     backSpeed: 50,
@@ -16,35 +17,35 @@ var date = new Date();
 var year = date.getFullYear();
 document.getElementById("year").innerHTML = year;
 
-const sections = document.querySelectorAll('.section');
+// const sections = document.querySelectorAll('.section');
 
-function debounce(func, wait = 5, immediate = true) {
-  let timeout;
-  return function() {
-    const context = this, args = arguments;
-    const later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
+// function debounce(func, wait = 5, immediate = true) {
+//   let timeout;
+//   return function() {
+//     const context = this, args = arguments;
+//     const later = function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     const callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
 
-function checkSlide() {
-  sections.forEach(section => {
-    const slideInAt = (window.scrollY + window.innerHeight) - section.offsetHeight / 2;
-    const sectionBottom = section.offsetTop + section.offsetHeight;
-    const isHalfShown = slideInAt > section.offsetTop;
-    const isNotScrolledPast = window.scrollY < sectionBottom;
-    if (isHalfShown && isNotScrolledPast) {
-      section.classList.add('is-active');
-    } else {
-      section.classList.remove('is-active');
-    }
-  });
-}
+// function checkSlide() {
+//   sections.forEach(section => {
+//     const slideInAt = (window.scrollY + window.innerHeight) - section.offsetHeight / 2;
+//     const sectionBottom = section.offsetTop + section.offsetHeight;
+//     const isHalfShown = slideInAt > section.offsetTop;
+//     const isNotScrolledPast = window.scrollY < sectionBottom;
+//     if (isHalfShown && isNotScrolledPast) {
+//       section.classList.add('is-active');
+//     } else {
+//       section.classList.remove('is-active');
+//     }
+//   });
+// }
 
-window.addEventListener('scroll', debounce(checkSlide));
+// window.addEventListener('scroll', debounce(checkSlide));
